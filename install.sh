b1eaabcc7a44b5eb164eb7b8e25c8362dfbf2b0c
Luke Attard <luke@rapiddata.com.au>
2024-11-02 08:26:32 +1100

    Update install.sh
    Added apt install for geoip-bin 
    
    TODO: add os detection and then use the correct package manager

--- install.sh
+++ install.sh
@@ -7,4 +7,7 @@ chmod 711 /usr/bin/fail2report
 mkdir /usr/lib/fail2report 2> /dev/null
 cp ${SCRIPTPATH}/*.awk /usr/lib/fail2report/
 
+printf "Installing geoip-bin\n\n"
+apt install geoip-bin
+
 printf "fail2report is now installed run fail2report --help for help\n\n"
